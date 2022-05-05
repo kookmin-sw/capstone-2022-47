121111cc9ffc8c8ffcf940baf20254b764250bd8
swlee22 <22kmcapstone@gmail.com>
2022-05-06 01:21:18 +0900

    today_empty page UI 수정

--- lib/pages/today/today_empty_widget.dart
+++ lib/pages/today/today_empty_widget.dart
@@ -6,20 +6,20 @@ class TodayEmpty extends StatelessWidget {
   const TodayEmpty({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
-    return Container(
-      color: Colors.yellow,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text('추가된 약이 없습니다.'),
-          SizedBox(height: smallSpace),
-          Text('약을 추가해주세요.', style: Theme.of(context).textTheme.subtitle1,
-          ),
-          SizedBox(height: smallSpace),
-          Icon(CupertinoIcons.arrow_down),
-          SizedBox(height: largeSpace),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const Center(child: Text('추가된 약이 없습니다.')),
+        const SizedBox(height: smallSpace),
+        Text(
+          '약을 추가해주세요',
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        const SizedBox(height: smallSpace),
+        const SizedBox(height: smallSpace),
+        const Icon(CupertinoIcons.arrow_down),
+        const SizedBox(height: largeSpace),
+      ],
     );
   }
 }
