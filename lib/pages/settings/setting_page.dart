79e2c7ff1966678e2e9c404673868f3dee6f3e3f
Sungrae <whvkek@naver.com>
2022-05-13 13:37:59 +0900

    앱설정화면 ui마무리

--- lib/pages/settings/setting_page.dart
+++ lib/pages/settings/setting_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../components/yaksok_constants.dart';
+import '../../models/my_medicine_header.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -28,120 +29,128 @@ class SettingPage extends StatelessWidget {
         ),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        margin: EdgeInsets.fromLTRB(15, 5, 15, 5),
         child: ListView(
           scrollDirection: Axis.vertical,
-          children: <Widget>[
+          children: [
+            MyMedicineHeader(),
+            const SizedBox(height: 8.0,),
+
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.bell,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "알림",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 5,),
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.hand_thumbsup,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "리뷰 작성하기",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 5,),
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.mail,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "의견 보내기",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 5,),
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.tag,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "앱 버전",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 5,),
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.trash,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "데이터 삭제",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 5,),
             ElevatedButton(
               onPressed: null,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
                     child: Icon(
                       CupertinoIcons.info,
-                      size: 45,
+                      size: 35,
                     ),
                   ),
                   Text(
                     "이용 약관",
-                    style: TextStyle(fontSize: 25),
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
               ),
